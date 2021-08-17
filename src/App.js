deeec7c1582e15500317da91b590260e5225d09a
iroDevs <ironegrolol@gamil.com>
2021-08-17 13:04:17 -0300

    e isso

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import { BrowserRouter , Switch , Route } from "react-router-dom";
 import Home from './pages/Home'
+import Favoritos from './pages/Favoritos';
 
 
 
@@ -12,7 +13,8 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Switch>
-      <Route path="/" render={(props)=> <Home {...props}  />} />
+      <Route exact path="/" render={(props)=> <Home {...props}  />} />
+       <Route exact path="/favoritos" render={(props)=> <Favoritos {...props}  />} />
 
 
       </Switch>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import * as api from '../services/api';
+import Favoritos from '../pages/Favoritos';
 
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/HomeStyle.css'
+
+import { Link } from 'react-router-dom';
 
 
 
@@ -15,12 +19,32 @@ export default class Home extends Component {
     this.state = {
       ListaInfo: [],
       loading: true,
+      favoritos:[],
     }
+    this.ClickButton = this.ClickButton.bind(this);
   }
 
 componentDidMount(){
   this.info();
 }
+
+ organizaArray(a){
+    
+  let jaVisto = {};
+    return a.filter(function(item) {
+        return jaVisto.hasOwnProperty(item) ? false : (jaVisto[item] = true);
+    });
+
+
+ }
+
+ ClickButton(event){
+   console.log(event.target.value);
+   const ID = event.target.value;
+   localStorage.setItem('favortitos'+ID,ID);
+
+  
+ }
 
   async info() {
   const personagens = await api.getInfoByApi('character');
@@ -37,55 +61,55 @@ componentDidMount(){
   
   render() {
     
-    const {ListaInfo} = this.state;
-    console.log(ListaInfo);
+    const {ListaInfo , loading} = this.state;
     
 
 
+    if(loading === false){
+
     return (
      <div>
+       <Link to="/favoritos">Favoritos</Link>
         <p>Home</p>
-      
+      <div className='H-container'>
       {
         ListaInfo.map((obj)=>{
           
           
-          return (
-            <ol>
+        return (
+          <ol>
               <li>
-             <Card style={{ width: '18rem' }}>
-
-            <Card.Img variant="top" src={obj.image} />
-
-          <Card.Body>
-              <Card.Title>{obj.name}</Card.Title>
-          <Card.Text>
-            <ul>
-              <li>Genero:{obj.gender} </li>
-               <li>Localização:{obj.location.name} </li>
-                <li>Especie:{obj.species} </li>
-
-
-              
-
-            </ul>
-          </Card.Text>
-
-          <Button variant="primary">Favorito</Button>
-        </Card.Body>
-      </Card>
-</li>
-</ol>
-
-
-
+                  <Card border='success' key={obj.id} text='light' bg='dark' style={{ width: '18rem', height: '520px' }}>
+                    <Card.Img variant="top" src={obj.image} />
+                  <Card.Body>
+                      <Card.Title>{obj.name}</Card.Title>
+                        <Card.Text>
+                            <ul>
+                              <li>Genero:{obj.gender} </li>
+                              <li>Localização:{obj.location.name} </li>
+                              <li>Especie:{obj.species} </li>
+                            </ul>
+                        </Card.Text>
+                  <Button variant="success" value={obj.id} onClick={this.ClickButton}>Favorito</Button>
+                </Card.Body>
+               </Card>
+            </li>
+        </ol>
+        
           );
         })
       }
 
  
-       
+       </div>
       </div>
     )
+     }
+     return (
+    <p>Carregando...</p>
+  )
   }
+
+  
+
 }
